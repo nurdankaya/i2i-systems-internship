a9596770d12b08772ba7ef8283460dffe037680c
nurdanKaya <nnurdankaya@gmail.com>
2021-07-30 10:38:02 +0300

    hazelcast presentation and basic demo

---
+++ hazelcast/demo-hazelcast/src/main/java/Member.java
@@ -0,0 +1,15 @@
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.map.IMap;
+
+public class Member {
+    public static void main(String[] args) {
+
+        HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+        IMap<Object,Object> map = instance.getMap("map");
+
+        for (int i =0; i < 1000; i++){
+            map.put(i,i);
+        }
+    }
+}
